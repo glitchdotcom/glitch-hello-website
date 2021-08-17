e696c4342b633aa416dfaa507183ebee7c1a10c3
Glitch (curved-plum-cuticle) <none>
2021-08-17 15:19:08 +0000

    🍏🍁 Updated with Glitch

--- sw.js
+++ sw.js
@@ -10,9 +10,7 @@ self.addEventListener("install", e => {
       return cache.addAll([
         "index.html",
         "style.css",
-        "script.js",
-        "https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2Fillustration.svg?v=1618177344016",
-        "https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140"
+        "script.js"     
       ]);
     })
   );
